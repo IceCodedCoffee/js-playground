4febaf03585e7fe281d7abedf5e3530c65007ca6
IceCodedCoffee <wolf.alexander.development@gmail.com>
2023-12-31 04:52:51 +0100

    feat: Add jQuery & Finish manipulate CSS

--- 9. jQuery - manipulating css/script.js
+++ 9. jQuery - manipulating css/script.js
@@ -10,4 +10,13 @@ $(() => {
     $("#btn03").click(() => {
         $("p").toggleClass("newStyle");
     });
+
+    $("div").css("background-color", "blue");
+
+    $("div.hinweis").css({
+        "background-color":"black",
+        "color": "white"
+    });
+
+    console.log($("p#wichtig").css("background-color"));
 });
